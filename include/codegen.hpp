6bf7e79045324a0e788cbcd0e54656cbda06a7c1
Sameer Jagdale <sameer.jagdale@mail.mcgill.ca>
2014-09-03 16:08:45 -0400

    Added header files to new repository.

---
+++ include/codegen.hpp
@@ -0,0 +1,344 @@
+/*
+ * codegen.hpp
+ *
+ *  Created on: 2013-03-15
+ *      Author: sjagda
+ */
+
+#ifndef CODEGEN_HPP_
+#define CODEGEN_HPP_
+
+#include "vraptor.hpp"
+#include <vector>
+#include<iostream>
+#include<string>
+#include<sstream>
+#include<stdlib.h>
+#include<set>
+#include<map>
+#include<unordered_map>
+#include<iterator>
+ #include<node-collector.hpp>
+//#include<memory>
+
+#define DIM_OFFSET 32
+#define SIZE_OFFSET 20
+#define DATA_OFFSET 16
+using std::vector;
+using std::string;
+using std::set;
+namespace VRaptor {
+
+
+/*! \brief Class to  hold the generated code.
+     The generated code is stored as a vector of strings. A context class object may also stored additional data that can be sent back .
+*/
+class Context {
+
+	vector<string> list; 
+	bool arrayFlag;
+	
+
+public:
+	Context();
+	void addStmt(const string stmt);//!<Add generated code to the list 
+	void addStmtVec(vector<string> );
+	vector<string> getAllStmt();    //<! returns vector of generated code
+	void setArrayFlag();
+	void resetArrayFlag();
+	bool isArray();
+};
+
+class BuiltinFunction {
+	private :
+		const std::string funcName;
+		bool arrayOutput;
+		bool varArgs;
+		bool hasScalOut;
+	public :
+		BuiltinFunction(std::string funcName,bool arrayOutput,bool varArgs=false,bool hasScalOut = false) : funcName(funcName), 
+		arrayOutput(arrayOutput),varArgs(varArgs),hasScalOut(hasScalOut) 
+		{
+		  
+		}
+		bool isArrayOutput() const{
+			return arrayOutput;
+		}
+		const std::string getFuncName() const {return funcName;}
+		bool hasVarArgs() {
+		  return varArgs;
+		}
+		bool isScalOut() { return hasScalOut;}
+}; //end of class BuiltinFunction.
+
+class BuiltinMapper {
+	private :
+		std::unordered_map<std::string, BuiltinFunction> builtinMap;
+	public :
+	void init() {
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("rand",BuiltinFunction("rand",true,true)));	
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("sqrt",BuiltinFunction("sqrt",true,false,true)));	
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("abs",BuiltinFunction("abs",true)));	
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("tan",BuiltinFunction("tan",true)));	
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("mmult",BuiltinFunction("mmult",true)));
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("mean",BuiltinFunction("mean",true)));
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("sum",BuiltinFunction("sum",true)));
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("min",BuiltinFunction("min",true)));
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("max",BuiltinFunction("max",true)));	
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("length",BuiltinFunction("length",false)));
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("floor",BuiltinFunction("floor",false)));
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("size",BuiltinFunction("size",false)));
+		builtinMap.insert(std::make_pair<std::string,BuiltinFunction>("colon",BuiltinFunction("colon",false)));
+	
+	}
+	bool contains(const std::string & funcName) {
+	  return  builtinMap.count(funcName) > 0;
+	}
+	
+	BuiltinFunction getBuiltin(const std::string & funcName) {
+	  if(contains(funcName)) {
+	   return builtinMap.find(funcName)->second;
+	  }
+	  std::cout<<"Entry not found"<<std::endl;
+	  return builtinMap.end()->second;
+	}
+}; 
+
+/*! \class VCompiler
+    \brief Main class for the static backend. 
+
+Provides methods which generate code for each node of VRIR. It takes a pointer of type VModule which is the root of the VRIR AST.   
+*/
+class VCompiler {
+private:
+	int maxTempVecId;
+	int maxTempIterId;
+	std::string tempVarStr;
+	std::string tempIterStr;
+	int structVarId;
+	VFunction *currFunction;
+	Context headerCntxt;
+	const std::string moduleName;
+	BuiltinMapper mapper;
+	NodeCollector collector;
+    std::set<LibCallExpr::Code> libCallSet;
+	//! Generates C++ code for a statement node. 
+	/*! Calls specialised methods depending on the type of the statement
+            \fn stmtTypeCodeGen
+	    \param StmtPtr is a pointer to the statement node.
+	    \param SymTable is a pointer to the function's symTable
+	*/
+	Context stmtTypeCodeGen(StmtPtr stmt, SymTable *symTable);  
+	//! Generates C++ code for expression node 
+	/*! Calls specialised methods depending on the type of the expression
+	    \fn exprTypeCodeGen 
+	    \param ExpressionPtr pointer to the expression node 
+	    \param SymTable pointer to the function's symbol table 
+	*/
+	Context exprTypeCodeGen(ExpressionPtr expr, SymTable *symTable,ExpressionPtr lExpr = NULL);
+	//! Generates C++ code for the VType . 
+	/*! Calls either scalarTypeCodeGen or arrayTypeCodeGen depending on whether whether it is a ArrayType or ScalarType. 
+	\param type is a pointer to a Vtype
+	\param symTable pointer to the function's symbl table
+	\param isPtr is passed to the scalarTypeCodegen to differentiate primitive scalar and scalars of type VrArrayPtr which are passed as parameters to the function. 
+		
+     */ 
+    Context vTypeCodeGen(VTypePtr type, SymTable *symTable,bool isPtr);
+    //! Generates C++ for scalar types. 
+    /*! The type generated is dependent on the boolean flag passed. 
+      The flag is set to false by default which means primitive C++ scalars are generated. VrArrayPtr is generated when the flag is set to true. 
+      \param vtype is a pointer to a ScalarType object. 
+      \param isPtr is a boolean which differentiates between primitive and VrArray* scalars. 
+     */
+    Context scalarTypeCodeGen(ScalarTypePtr vtype,bool isPtr );
+    //! Generates C++ for Array Types. 
+    /*! \fn arrayTypeCodeGen
+     * \param type is the ArrayType pointer.
+     \param symTable is a pointer to the function's symbol table. 
+     \param isPtr differentiates between primitive and VrArray type arrays.   
+     */
+    Context arrayTypeCodeGen(ArrayTypePtr type, SymTable *symTable,bool);
+    //! generates a set of scalars which are wrapped inside VrArrayPtr. 
+    /*! These scalars are found in the function parameters.
+      \param  func is a pointer to the current VFunction.
+     */
+    void genScalSet(VFunction* func);
+    //! generates a string depending on the VType. For blas libraries
+    /*! Blas functions for VRIR differ on depending on the returning type/input parameters.
+     */ 
+    string getTypeStr(VTypePtr vtype);	
+    string getTypeStr(ScalarTypePtr vtype);	
+    string getTypeStr(ArrayTypePtr vtype);	
+    string genDataStr(VTypePtr vtype,string name);
+    string genDimStr(VTypePtr vtype,string name);
+    string genDataMacroStr(VTypePtr vtype,string name);
+    string genDimMacroStr(VTypePtr vtype,string name);
+    std::string genCblasOrder(ArrayTypePtr vtype);
+    std::string genCblasTrans();
+    std::string getNdims(ArrayTypePtr);
+    bool enableOpenMP;
+    bool enableSse;
+    set<string> *scalSet;
+    std::string itoa(int num);
+    bool isInt(ExpressionPtr expr);
+    bool isInt(ScalarTypePtr type);
+    bool isMatMultCall(LibCallExprPtr expr);
+    Context getOriginalArrStr(NameExprPtr expr, SymTable * symTable);
+    Context handleSpecMatMultCall(LibCallExprPtr expr, SymTable *symTable,ExpressionPtr lhsExpr);
+    std::string getIndexDiffStr();
+    Context generateArgs(VFunction*func);
+    Context generateReturnType(VFunction *func);
+    Context generateTypeDec(VFunction *);
+    bool requireOriginalArr(AssignStmtPtr stmt);
+    bool isArrayCopy(AssignStmtPtr stmt);
+    std::string genCopyExpr(std::string name , int ndims);
+    std::string genCopyExpr(NameExprPtr expr,SymTable * symTable);
+    std::string genIndexStr(IndexExprPtr expr, SymTable *symTable);
+    bool isRowMajor(int id, SymTable *symTable);
+    bool isSlice(IndexExprPtr expr);
+    string genRangeFuncStr(VRange * range,SymTable * symTable);
+    std::string genRangeFuncStr(ExpressionPtr expr, SymTable *symTable);
+    //TODO: remove boolean flag once specialisation for slice set implemented. 
+    std::string genSliceStr(IndexExprPtr expr, SymTable *symTable);
+    std::string  handleArraySlicing(IndexExprPtr expr, SymTable *symTable);
+    bool scalarArgs(FunCallExprPtr expr);
+    bool hasArrayArg(FunCallExprPtr expr);
+    std::string genTempVec();
+    std::string genIterVar();
+    std::string genFuncStructName(VFunction * func);
+    std::string genStructData();	
+    std::string genConstructor(VFunction *func,SymTable *symTable);
+    std::string genRetStruct(VFunction * func);	
+    bool hasMultRet(VFunction* func);
+    bool hasMultRet(StmtListPtr stmtList);
+    std::string getBoundCheckFuncStr();
+    Context genBoundCheckStmt(IndexExprPtr expr,SymTable * symTable,bool onLhs = false);
+    Context genSpecialisedBoundCheck(IndexExprPtr expr, SymTable *symTable, bool onLhs);
+    std::string genBoundCheckHeader(IndexExprPtr expr, SymTable* symTable);
+    std::string genSpecNegativeIndexStr(IndexExprPtr expr, SymTable *symTable);
+    bool  canSpecialiseNegativeIndex(IndexVec vec); 
+    bool canSpecialiseArraySlice(IndexVec vec);
+    std::string genSpecArraySliceStr(IndexExprPtr expr, SymTable *symTable);
+    bool  canSpecBoundCheckStmt(IndexVec vec); 
+    bool hasColon(IndexExprPtr expr);
+    bool isOnLhs(IndexExprPtr expr);
+    Context handleArraySliceSet(IndexExprPtr lhsExpr, ExpressionPtr expr, SymTable* symTable);
+    Context handleSpecArraySliceSet(IndexExprPtr lhsExpr, ExpressionPtr expr, SymTable *symTable);
+public:
+    bool isNegativeIndex( IndexExprPtr expr);
+    std::vector<string> getIndexTemps( int n );
+    VCompiler():tempVarStr("vrTempVec"),tempIterStr("vrTempIter"),moduleName("default"){
+        scalSet = new set<string>();
+        maxTempVecId = 0;
+        maxTempIterId = 0;
+        structVarId = 0;
+        initLibCallSet();
+
+	}
+
+	VCompiler(std::string moduleName):tempVarStr("vrTempVec"),tempIterStr("vrTempIter"),moduleName(moduleName){
+		scalSet = new set<string>();
+		maxTempVecId = 0;
+		maxTempIterId = 0;
+    	structVarId = 0;
+        initLibCallSet();
+	}
+	~VCompiler(){
+		delete scalSet;
+	}
+	typedef enum LoopDirection {
+		COUNT_UP,
+		COUNT_DOWN, 
+		UNKNOWN
+	}LoopDirection;
+	bool getSseFlag();
+	void setSseFlag(bool);
+	bool getOpenmpFlag();
+	void setOpenMpFlag(bool);
+	Context funcCodeGen(VFunction *func);
+	Context stmtCodeGen(StmtPtr stmt, SymTable *symTable);
+	Context forStmtCodeGen(ForStmtPtr stmt, SymTable *symtable);
+	Context assignStmtCodeGen(AssignStmtPtr stmt, SymTable *symTable);
+	Context nameExprCodeGen(NameExprPtr expr, SymTable *symTable);
+    bool isNegativeConst(IndexStruct index);
+	Context plusExprCodeGen(PlusExprPtr expr, SymTable *symTable);
+	Context domainExprCodeGen(DomainExprPtr expr, SymTable *symTable);
+    Context dimExprCodeGen(DimExprPtr expr, SymTable *symTable); 
+	Context constExprCodeGen(ConstExprPtr expr, SymTable *symTable);
+	Context stmtListCodeGen(StmtListPtr stmt, SymTable *symTable);
+	Context indexExprCodeGen(IndexExprPtr expr, SymTable *symTable);
+	Context minusExprCodeGen(MinusExprPtr expr, SymTable *symTable);
+	Context matMultCallCodeGen(LibCallExprPtr expr, SymTable *symTable,ExpressionPtr lhsExpr=NULL);
+	Context matPlusCallCodeGen(LibCallExprPtr expr, SymTable *symTable,ExpressionPtr lExpr=NULL);
+	Context matMinusCallCodeGen(LibCallExprPtr expr, SymTable *symTable,ExpressionPtr lExpr = NULL);
+	Context matLDivCallCodeGen(LibCallExprPtr expr, SymTable *symTable, ExpressionPtr lExpr = NULL);
+	Context matRDivCallCodeGen(LibCallExprPtr expr,SymTable *symTable,ExpressionPtr lExpr = NULL);
+	Context matTransCallCodeGen(LibCallExprPtr expr, SymTable *symTable,ExpressionPtr lExpr = NULL);
+	Context elemMultCallCodeGen(LibCallExprPtr expr, SymTable *symTable,ExpressionPtr lExpr = NULL);
+	Context elemDivCallCodeGen(LibCallExprPtr expr, SymTable * symTable,ExpressionPtr lExpr = NULL);
+	Context returnStmtCodeGen(ReturnStmtPtr stmt, SymTable *symTable);
+	Context moduleCodeGen(VModule *vm);
+	Context breakStmtCodeGen(BreakStmtPtr stmt);
+	Context continueStmtCodeGen(ContinueStmtPtr stmt);
+	Context ifStmtCodeGen(IfStmtPtr stmt, SymTable * symTable);
+	Context whileStmtCodeGen(WhileStmtPtr stmt, SymTable *symTable);
+	//Context refOpStmtCodeGen(RefOpStmtPtr stmt, SymTable *symTable);
+	Context pForStmtCodeGen(PforStmtPtr stmt, SymTable *symTable);
+	Context allocExprCodeGen(AllocExprPtr expr, SymTable *symTable);
+	Context binaryExprCodeGen(BinaryExprPtr expr, SymTable *symTable);
+	Context multExprCodeGen(MultExprPtr expr, SymTable *symTable);
+	Context divExprCodeGen(DivExprPtr expr, SymTable *symTable);
+	Context geqExprCodeGen(GeqExprPtr expr, SymTable *symTable);
+	Context eqExprCodeGen(EqExprPtr expr, SymTable *symTable);
+	Context gtExprCodeGen(GtExprPtr expr, SymTable *symTable);
+	Context ltExprCodeGen(LtExprPtr expr, SymTable *symTable);
+	Context leqExprCodeGen(LeqExprPtr expr, SymTable *symTable);
+	Context neqExprCodeGen(NeqExprPtr expr, SymTable *symTable);
+	Context andExprCodeGen(AndExprPtr expr, SymTable *symTable);
+	Context orExprCodeGen(OrExprPtr expr, SymTable *symTable);
+	Context notExprCodeGen(NotExprPtr expr, SymTable *symTable); //to be written
+	Context funCallExprCodeGen(FunCallExprPtr expr, SymTable *symTable);
+	Context libCallExprCodeGen(LibCallExprPtr expr, SymTable *symtable,ExpressionPtr lExpr=NULL);
+	Context negateExprCodeGen(NegateExprPtr expr, SymTable *symTable);
+	std::string getMatTypeStr(VTypePtr vtype);
+	std::string generateMatClassStr(LibCallExprPtr expr, SymTable *symTable);
+	std::string generateMatClassStr(VTypePtr vtype);
+	std::string generateArgs(ExpressionPtrVector args,SymTable *symTable,bool checkIsInt=false, std::string cast="");
+	Context catCallCodeGen(FunCallExprPtr expr, SymTable *symTable);
+	std::vector<LoopDirection> getLoopDirections(DomainExprPtr expr, SymTable *symTable);
+	Context handleMultReturns(ReturnStmtPtr stmt,SymTable *symTable);
+	std::string genStructVarStr(VFunction *func);
+	bool hasMultRet(FunCallExprPtr expr);
+	Context genBoundCheckStmt(StmtPtr stmt,SymTable * symTable, bool onLhs=false);
+	std::string genFuncStructName(std::string funcName);
+	std::string genFuncStructName(FunCallExprPtr expr);
+	std::string genStructVarStr(std::string structType);
+	Context handleMultLhs(AssignStmtPtr stmt, SymTable* symtable);
+	Context tupleExprCodeGen(TupleExprPtr expr, SymTable* symTable);
+    Context handleScalarLibcall(ExpressionPtr lExpr, ExpressionPtr rExpr, SymTable *symTable);
+    bool isScalarLibcall(AssignStmtPtr stmt, SymTable *symTable);
+	void genHeaderFile(VModule *vm);
+	Context getHeaderContext() { return headerCntxt;}
+	void setHeaderContext(Context cntxt) {headerCntxt = cntxt;}
+	std::string genFuncHeader(VFunction *func);
+	bool isBuiltin(const std::string & funcName);
+	bool isComplex(ExpressionPtr expr);
+    std::string genNegativeIndexStr(IndexExprPtr expr, SymTable *symTable);
+    bool isSpecLibCall(AssignStmtPtr stmt);
+    void initLibCallSet();
+	void setCollector(NodeCollector& nc) {
+		collector = nc;
+	}
+	NodeCollector getCollector() const {
+		return collector;
+	}
+	bool hasIndexExpr(StmtPtr stmt);
+    bool isNegativeIndex( StmtPtr stmt);
+    IndexSet* getIndexSet(StmtPtr stmt);
+	std::string getSpecBoundCheckFuncStr();
+	std::string genSpecBoundCheckHeader(IndexExprPtr expr, SymTable* symTable);
+};
+
+
+}//end of VRaptor namespace 
+#endif /* CODEGEN_HPP_ */
---
+++ include/node-collector.hpp
@@ -0,0 +1,96 @@
+#ifndef __NODE_COLLECTOR__H
+#define __NODE_COLLECTOR__H 
+#include <vraptor.hpp>
+#include <unordered_map>
+#include <unordered_set>
+
+using std::unordered_set;
+using std::unordered_map;
+
+namespace VRaptor{
+      typedef std::unordered_set<IndexExprPtr> IndexSet;
+      typedef std::unordered_map<StmtPtr,IndexSet*> IndexMap;
+  class NodeCollector {
+    private :
+      IndexMap stmtToIndexExprMap;
+      StmtPtr currStmt;
+      bool onLhs;
+      IndexSet lhsIndexSet;
+	  IndexSet indexColonSet; 
+      void addToMap(StmtPtr currStmt,IndexExprPtr indexExpr);
+      void addToColonSet(IndexExprPtr index);
+	  void addToLhsSet(IndexExprPtr index);
+    public :
+      void caseNode(Node* node);
+      void caseModule(VModule * node);
+      void caseFunction(VFunction *node);
+      void caseExpr(ExpressionPtr node);
+      void caseRealConstExpr(ConstExprPtr node);
+      void caseNameExpr(NameExprPtr node);
+      void caseBinaryExpr(BinaryExprPtr node);
+      void casePlusExpr(PlusExprPtr node);
+      void caseMinusExpr(MinusExprPtr node);
+      void caseMultExpr(MultExprPtr node);
+      void caseDivExpr(DivExprPtr node);
+      void caseGtExpr(GtExprPtr node);
+      void caseGeqExpr(GeqExprPtr node);
+      void caseLtExpr(LtExprPtr node);
+      void caseLeqExpr(LeqExprPtr node);
+      void caseEqExpr(EqExprPtr node);
+      void caseNeqExpr(NeqExprPtr node);
+      void caseAndExpr(AndExprPtr node);
+      void caseOrExpr(OrExprPtr node);
+      void caseUnaryExpr(UnaryExprPtr node);
+      void caseNotExpr(NotExprPtr node);
+      void caseNegateExpr(NegateExprPtr node);
+      void caseFuncallExpr(FunCallExprPtr node);
+      void caseVRange(VRange* node);
+      void caseIndexExpr(IndexExprPtr node);
+      void caseDomainExpr(DomainExprPtr node);
+      void caseTupleExpr(TupleExprPtr node);
+      void caseMapExpr(MapExprPtr);
+      void caseLibCallExpr(LibCallExprPtr expr);
+      void caseDimExpr(DimExprPtr expr);
+      void caseDimVecExpr(DimvecExprPtr expr);
+      void caseAllocExpr(AllocExprPtr expr);
+      void caseImagExpr(ImagExprPtr expr);
+      void caseRealExpr(RealExprPtr expr);
+      void caseCastExpr(CastExprPtr expr);
+      void caseStmt(StmtPtr node);
+      void caseStmtList(StmtListPtr node);
+      void caseBoundCheckStmt(BoundCheckStmtPtr node);
+      void caseAssignStmt(AssignStmtPtr node);
+      void caseIfStmt(IfStmtPtr node);
+      void caseForStmt(ForStmtPtr node);
+      void casePforStmt(PforStmtPtr node);
+      void caseWhileStmt(WhileStmtPtr node);
+      void caseBreakStmt(BreakStmtPtr node);
+      void caseContinueStmt(ContinueStmtPtr node);
+      void caseRefOpStmt(RefOpStmtPtr node);
+      void caseReturnStmt(ReturnStmtPtr node);
+      void prettyPrint();
+      bool containsStmt( StmtPtr stmt ) {
+            return stmtToIndexExprMap.count(stmt) > 0;
+      }
+      IndexSet* getIndexSet(StmtPtr stmt){
+            if(containsStmt(stmt)) {
+                  return stmtToIndexExprMap.find(stmt)->second;
+            }
+            return NULL;
+      }
+	  bool isOnLhs(IndexExprPtr expr) {
+			return lhsIndexSet.count(expr) > 0;
+	  }
+	  bool hasColon(IndexExprPtr expr) {
+			return indexColonSet.find(expr) != indexColonSet.end(); 
+	  } 
+      void analyze(VModule *);
+      ~NodeCollector(){
+        stmtToIndexExprMap.clear();
+      }
+      IndexMap getMap()const { return stmtToIndexExprMap;}
+
+  };
+
+}
+#endif

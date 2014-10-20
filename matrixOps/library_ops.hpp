2bc11e2e07ae47b38d42d9bd656d05249d0f2683
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2014-10-20 12:42:32 -0400

    Added template file to library_ops
    Allows me make the file visible to outside functions.

--- matrixOps/library_ops.hpp
+++ matrixOps/library_ops.hpp
@@ -13,6 +13,7 @@
 #include<library_ops.tpp>
 #include<negative_index_handler.tpp>
 #include<boundcheck.tpp>
+#include<checkDims.tpp>
 #define mod(x,y) ((long)x%(long)y)
 bool isVector(dim_type* dims,dim_type ndims);
 bool isScalar(dim_type* dims,dim_type ndims);

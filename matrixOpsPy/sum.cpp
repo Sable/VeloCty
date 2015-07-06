b275efd909a2378ede47881eee5545e66ade8e81
sameerjagdale <sameer.jagdale@mail.mcgill.ca>
2015-07-06 15:32:16 -0400

    Modified sum to support the NumPy Array.
    Calls to the numpy functions were made.

--- matrixOpsPy/sum.cpp
+++ matrixOpsPy/sum.cpp
@@ -1,29 +1,17 @@
 #include "library_ops.hpp"
-#include "sum.tpp"
+
 double sum_scalar(VrArrayPtrF64 A) {
-	double out;
-	dim_type  numel = getNumElem(A.dims,A.ndims);
-	out = sum<double>(numel,VR_GET_DATA_F64(A));
-	return out;
+    return *(PyArray_DATA(PyArray_Sum(A,0,PyArray_Type(A),NULL));
 } 
 
 float sum_scalar(VrArrayPtrF32 A) {
-	float out;
-	dim_type  numel = getNumElem(A.dims,A.ndims);
-	out = sum<float>(numel,VR_GET_DATA_F32(A));
-	return out;
+    return *(PyArray_DATA(PyArray_Sum(A,0,PyArray_Type(A),NULL));
 } 
 
 double complex sum_scalar(VrArrayPtrCF64 A) {
-	double complex out;
-	dim_type  numel = getNumElem(A.dims,A.ndims);
-	out = sum<double complex>(numel,VR_GET_DATA_CF64(A));
-	return out;
+    return *(PyArray_DATA(PyArray_Sum(A,0,PyArray_Type(A),NULL));
 } 
 
 float complex sum_scalar(VrArrayPtrCF32 A) {
-	float complex out;
-	dim_type  numel = getNumElem(A.dims,A.ndims);
-	out = sum<float complex>(numel,VR_GET_DATA_CF32(A));
-	return out;
+    return *(PyArray_DATA(PyArray_Sum(A,0,PyArray_Type(A),NULL));
 } 
